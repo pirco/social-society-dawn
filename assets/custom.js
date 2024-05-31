2248bd2d8393f013f1d49dd0939fcb9173ad23b5
Anton Lastochkin <lastant@gmail.com>
2024-05-31 00:29:01 -0500

    Clips

--- assets/custom.js
+++ assets/custom.js
@@ -1,11 +1,25 @@
-const listItems = document.querySelectorAll('.mega-menu__list > li:not(:first-child)');
-const firstChild = document.querySelector('.mega-menu__list > li:first-child');
-listItems.forEach(item => {
-  item.addEventListener('mouseenter', () => {
-    firstChild.classList.remove('hover-active');
+document.addEventListener('DOMContentLoaded', function () {
+
+  const listItems = document.querySelectorAll('.mega-menu__list > li:not(:first-child)');
+  const firstChild = document.querySelector('.mega-menu__list > li:first-child');
+  listItems.forEach(item => {
+    item.addEventListener('mouseenter', () => {
+      firstChild.classList.remove('hover-active');
+    });
+
+    item.addEventListener('mouseleave', () => {
+      firstChild.classList.add('hover-active');
+    });
   });
 
-  item.addEventListener('mouseleave', () => {
-    firstChild.classList.add('hover-active');
+  const mediaElements = document.querySelectorAll('.product-card-wrapper');
+  mediaElements.forEach(function (element) {
+    element.addEventListener('mouseenter', function () {
+      element.classList.add('hovered');
+      document.querySelector('#clip-balloon-animation path animate').beginElement();
+    });
+    element.addEventListener('mouseleave', function () {
+      element.classList.remove('hovered');
+    });
   });
 });
