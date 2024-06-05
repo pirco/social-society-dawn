e81d20f26353c1674c5eb7a3cc4667e9b22d9854
Anton Lastochkin <lastant@gmail.com>
2024-06-05 15:56:40 -0500

    Credits

--- assets/custom.js
+++ assets/custom.js
@@ -29,3 +29,15 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 });
+
+var creditsElements = document.querySelectorAll('.credits');
+creditsElements.forEach(function (creditsElement) {
+  creditsElement.addEventListener('click', function () {
+    var firstSpan = creditsElement.querySelector('span:nth-child(1)');
+    var secondSpan = creditsElement.querySelector('span:nth-child(2)');
+    if (firstSpan && secondSpan) {
+      firstSpan.style.display = 'none';
+      secondSpan.style.display = 'inline-block';
+    }
+  });
+});
