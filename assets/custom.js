507c3427338f0d27f143a753548d56f4a59051c8
Anton Lastochkin <lastant@gmail.com>
2024-06-07 19:32:57 -0500

    Mega menu two columns; #1

--- assets/custom.js
+++ assets/custom.js
@@ -1,17 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-
-  const listItems = document.querySelectorAll('.mega-menu__list > li:not(:first-child)');
-  const firstChild = document.querySelector('.mega-menu__list > li:first-child');
-  listItems.forEach(item => {
-    item.addEventListener('mouseenter', () => {
-      firstChild.classList.remove('hover-active');
-    });
-
-    item.addEventListener('mouseleave', () => {
-      firstChild.classList.add('hover-active');
-    });
-  });
-
   const mediaElements = document.querySelectorAll('[class*="__featured_collection"] .product-card-wrapper');
   mediaElements.forEach(function (element) {
     element.addEventListener('mouseenter', function () {
